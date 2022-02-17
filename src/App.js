c8c44f83a413dc74e2b86d7a5837ae000789b418
osemko <osemko@life360.com>
2022-02-17 20:13:23 +0200

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Man Boo
+          React is powerfull
         </a>
       </header>
     </div>
